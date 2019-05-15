370e3162a8ee21a841d29ac46747cbff69b6c712
amshel <amshelhack3r@gmail.com>
2019-05-15 17:02:03 +0000

    Main style page for the app

---
+++ lib/Styles/styles.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+abstract class styles{
+  static const headingStyle = TextStyle(
+    fontSize: 18,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+    fontFamily: "Roboto"
+  );
+}
